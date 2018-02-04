5d0d83bd333045abeca6e058d6dd2fb72581da1e
MatthewCranford <matthewcranford314@gmail.com>
2018-02-03 17:26:45 -0800

    Work on algo

--- wherefore-art-thou.js
+++ wherefore-art-thou.js
@@ -7,13 +7,23 @@ function whatIsInAName(collection, source) {
 //   console.log(collection[0].hasOwnProperty("first"));
   
   // Check each object in collection for the value of each of its keys 
-  for(var object in collection) {
+  for (var object in collection) {
     console.log(collection[object]);
     console.log(Object.keys(source));
-    console.log(collection[object].hasOwnProperty(Object.keys(source)[0]));
-    if (collection[object].hasOwnProperty(Object.keys(source)) && collection[object][Object.keys(source)] === source[Object.keys(source)]) {
-      arr.push(collection[object]);
+    for (var key in Object.keys(source)) {
+      console.log(Object.keys(source)[key]);
+      if (collection[object].hasOwnProperty(Object.keys(source)[key]) && collection[object][Object.keys(source)[key]] === source[Object.keys(source)[key]]  ) {
+        console.log(collection[object][Object.keys(source)[key]]);
+        console.log(source[Object.keys(source)[key]]);
+        arr.push(collection[object]);
+        console.log(arr);
+        break;
+      }     
     }
+//     console.log(collection[object].hasOwnProperty(Object.keys(source)[0]));
+//     if (collection[object].hasOwnProperty(Object.keys(source)) && collection[object][Object.keys(source)] === source[Object.keys(source)]) {
+//       arr.push(collection[object]);
+//     }
    
   }
   // If key/value pair is equal, add to array
@@ -21,5 +31,4 @@ function whatIsInAName(collection, source) {
   // Only change code above this line
   return arr;
 }
-
 whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
