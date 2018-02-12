b44f1803de0b0f31339f1ef4d1c5c52d29f01e27
Matt <matthewcranford314@gmail.com>
2018-02-12 12:42:41 -0800

    Solved steamroller

--- steamroller.js
+++ steamroller.js
@@ -1,26 +1,62 @@
 
-function checkSubArr(subArr) {
-  if (Array.isArray(arr[i])) {
-      flatArr.push(arr[i][0]);
-    }
-}
+
+
+// function checkSubArr(subArr) {
+//   console.log("entering subArr", subArr);
+//    var tempFlatArr = [];
+
+//   for(var subItem=0; subItem<subArr.length; subItem++) {
+//     console.log(subArr,subArr[subItem],Array.isArray(subArr[subItem]));
+//     if (Array.isArray(subArr[subItem])) {
+//       checkSubArr(subArr[subItem]);
+//         }
+//     else {
+//       tempFlatArr.push(subArr[subItem]);
+//     }
+//     console.log("tempFlatArr:",tempFlatArr);
+//     return tempFlatArr;
+//   }
+// }
 
 function steamrollArray(arr) {
   // I'm a steamroller, baby
   
-  // store flat array
   var flatArr = [];
   
-  // for each item in arr
-  for(var i = 0; i < arr.length; i++) {
-    //if item is an array
-    if (Array.isArray(arr[i]) && checkSubArr(arr[i]) === false) {
-      flatArr.push(arr[i][0]);
+//   console.log("Entering Loop for:", arr);  
+  for(var arrItem = 0; arrItem < arr.length; arrItem++) {
+//     console.log("arr loop item:",arr[arrItem],"array?:",Array.isArray(arr[arrItem]));
+    // if item is an array
+    if (Array.isArray(arr[arrItem])) {
+      var subArr = arr[arrItem];
+      for (var subItem = 0; subItem < subArr.length; subItem++) {
+//         console.log("sub loop item:",subArr[subItem]);
+        if (Array.isArray(subArr[subItem])) {
+          var subSubArr = subArr[subItem];
+          for (var subSubItem = 0; subSubItem < subSubArr.length; subSubItem++) {
+            if (Array.isArray(subSubArr[subSubItem])) {
+              var subSubSubArr = subSubArr[subSubItem];
+              for (var subSubSubItem = 0; subSubSubItem < subSubSubArr.length; subSubSubItem++) {
+                flatArr.push(subSubSubArr[subSubSubItem]);
+              }
+            }
+            else {
+              flatArr.push(subSubArr[subSubItem]);
+            }
+          }
+        }
+        else {
+          flatArr.push(subArr[subItem]);
+        }
+      }
     }
-    console.log(arr,Array.isArray(arr[i]));
+    else {
+      flatArr.push(arr[arrItem]);
+    }
   }
+//   console.log("flatArr =" ,flatArr);
   return flatArr;
 }
 
-// steamrollArray([1, [2], [3, [[4]]]]);
-steamrollArray([[["a"]], [["b"]]]);
+steamrollArray([1, [2], [3, [[4]]]]);
+// steamrollArray([[["a"]], [["b"]]]);
